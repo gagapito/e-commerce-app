d9e2e81ea5be776e41f23cf662e7b8b00e1ac391
gagapito <garien2k@gmail.com>
2022-04-25 16:48:51 -0700

    4th commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
 
       <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
-        <span className='cart-item=qty'>{totalQuantities}</span>
+        <span className='cart-item-qty'>{totalQuantities}</span>
       </button>
 
       {showCart && <Cart />}
