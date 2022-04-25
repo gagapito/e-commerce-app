82097ac3f81dec4d64144e43293eb4b00c3a8266
gagapito <garien2k@gmail.com>
2022-04-25 11:07:18 -0700

    3rd commit - ecommerce app implementation

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -3,22 +3,22 @@ import Link from 'next/link'
 
 import { urlFor } from '../lib/client'
 
-const HeroBanner = ({ heroBanner }) => {
+const HeroBanner = ({ heroBanner: { largeText1, smallText, midText, product, buttonText, desc, image } }) => {
   return (
     <div className='hero-banner-container'>
       <div>
-        <p className='beats-solo'>{heroBanner.smallText}</p>
-        <h3>{heroBanner.midText}</h3>
-        <h1>{heroBanner.largeText1}</h1>
-        <img src={urlFor(heroBanner.image)} alt='shoes' className='hero-banner-image' />
+        <p className='beats-solo'>{smallText}</p>
+        <h3>{midText}</h3>
+        <h1>{largeText1}</h1>
+        <img src={urlFor(image)} alt='shoes' className='hero-banner-image' />
 
         <div>
-          <Link href={`/product/${heroBanner.product}`}>
-            <button type='button'>{heroBanner.buttonText}</button>
+          <Link href={`/product/${product}`}>
+            <button type='button'>{buttonText}</button>
           </Link>
           <div className='desc'>
             <h5>Description</h5>
-            <p>{heroBanner.desc}</p>
+            <p>{desc}</p>
           </div>
         </div>
       </div>
